c66f83705bae2eecc73be0187f9933318bbffd4a
Ilia Konev <ilikonev@luxoft.com>
2024-08-22 23:40:36 +0200

    Added module resource group

---
+++ azurerm_resource_group/variables.tf
@@ -0,0 +1,10 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The location where the resource group will be created"
+  type        = string
+  default     = "East US"
+}
